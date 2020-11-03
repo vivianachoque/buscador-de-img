ab2dbd45c656a1e5dedda858d9fa620858d46b48
vivianachoque <vivianaachoque@gmail.com>
2020-11-03 20:59:23 -0300

    Falta exportar el componente Buscador

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Buscador from './componentes/Buscador'
+
 function App() {
   return (
     <div className="app container">
       <div className="jumbotron">
       <p className="lead text-center">Buscador de imagenes</p>
+      <Buscador/>
     </div>
     </div>
   );
