9964a9458758295949cc5b19cf8a47f0359a08f1
vivianachoque <vivianaachoque@gmail.com>
2020-11-04 02:48:49 -0300

    arregle errores de codigo en componente Buscador

--- src/componentes/Buscador/index.js
+++ src/componentes/Buscador/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class Buscador extends Component {
- busquedaRef = React.createContext.Ref();
+ 
+    busquedaRef = React.createRef();
+
+    obtenerDatos = (e) => {
+     e.preventDefault();
+
+     console.log(this.busquedaRef.current.value);
+    }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.obtenerDatos}>
         <div className="row">
           <div className="form-group col-md-8">
-            <input ref={this.busquerdaRef}
+            <input ref={this.busquedaRef}
               type="text"
               className="form-control form-control-lg"
               placeholder="Busca tu Imagen. Ejemplo: Guitarra"
