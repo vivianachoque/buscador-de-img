d97d5c66d7f466e0ff379478cc638a8bb98f7931
vivianachoque <vivianaachoque@gmail.com>
2020-11-04 00:57:17 -0300

    logro configurar eslint y agrego buscadaRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Buscador from "./componentes/Buscador";
+import React from 'react';
+import Buscador from './componentes/Buscador';
 
 function App() {
   return (
--- src/componentes/Buscador/index.js
+++ src/componentes/Buscador/index.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Buscador extends Component {
+ busquedaRef = React.createContext.Ref();
   render() {
     return (
       <form>
         <div className="row">
           <div className="form-group col-md-8">
-            <input
+            <input ref={this.busquerdaRef}
               type="text"
               className="form-control form-control-lg"
-              placeholder="Busca tu Imagen. Ejemplo: Guitarra"/>
+              placeholder="Busca tu Imagen. Ejemplo: Guitarra"
+            />
           </div>
           <div className="form-group col-md-4">
             <input
               type="submit"
               className="btn btn-lg btn-danger btn-block"
-              value="Buscar..."/>
+              value="Buscar..."
+            />
           </div>
         </div>
       </form>
