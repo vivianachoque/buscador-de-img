b37234fa4eb1c08df22d4bd54435246501c8e626
vivianachoque <vivianaachoque@gmail.com>
2020-11-03 21:11:50 -0300

    Agrego boton buscar...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Buscador from './componentes/Buscador'
+import Buscador from "./componentes/Buscador";
 
 function App() {
   return (
     <div className="app container">
       <div className="jumbotron">
-      <p className="lead text-center">Buscador de imagenes</p>
-      <Buscador/>
-    </div>
+        <p className="lead text-center">Buscador de imagenes</p>
+        <Buscador />
+      </div>
     </div>
   );
 }
--- src/componentes/Buscador/index.js
+++ src/componentes/Buscador/index.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Buscador extends Component {
-    render() { 
-        return ( 
-            <form>
-                <div className="row">
-                    <div className="form-group col-md-8">
-                        <input type="text" className="form-control form-control-lg" placeholder="Busca tu Imagen, Ejemplo: Guitarra"/>
-                    </div>
-                </div>
-            </form>
-         );
-    }
+  render() {
+    return (
+      <form>
+        <div className="row">
+          <div className="form-group col-md-8">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Busca tu Imagen. Ejemplo: Guitarra"/>
+          </div>
+          <div className="form-group col-md-4">
+            <input
+              type="submit"
+              className="btn btn-lg btn-danger btn-block"
+              value="Buscar..."/>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
- 
+
 export default Buscador;
