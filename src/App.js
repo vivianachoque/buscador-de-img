867456a6a01406cf2db7cf2f74e588f6bebd8543
vivianachoque <vivianaachoque@gmail.com>
2020-11-04 13:54:11 -0300

    continuo con funcion de busqueda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Buscador from './componentes/Buscador';
 
+datosBusqueda = (termino) => {
+    console.log(termino);
+}
+
 function App() {
   return (
     <div className="app container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imagenes</p>
-        <Buscador />
+        <Buscador
+        datosBusqueda = {this.datosBusqueda}
+         />
       </div>
     </div>
   );
